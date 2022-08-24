3bf3039df9ddd581023dd9391c9f1947ace1cd91
*Thanh <*thanhminh339968az@gmail.com*>
2022-08-24 15:07:03 +0700

    add

---
+++ Bai-15,16,17/src/index.js
@@ -0,0 +1,43 @@
+const path = require('path')
+const express = require('express')
+const morgan = require('morgan')
+const handlebars = require('express-handlebars');
+const app = express()
+const port = 3000
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.urlencoded({
+  extended:true
+}));
+app.use(express.json());
+
+//HTTP logger
+//app.use(morgan('combined'))
+
+// Template engine 
+app.engine('hbs', handlebars({
+    extname: '.hbs'
+}));
+app.set('view engine', 'hbs');
+app.set('views', path.join(__dirname, 'resources/views'));
+
+app.get('/', (req, res) => {
+  res.render('home');
+})
+
+app.get('/new', (req, res) => {
+  res.render('new');
+})
+
+app.get('/search', (req, res) => {
+  res.render('search');
+})
+
+app.post('/search', (req, res) => {
+  res.send('search');
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
